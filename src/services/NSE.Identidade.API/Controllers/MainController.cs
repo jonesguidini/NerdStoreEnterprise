1fe568249a80ebac5a55157c07288838bb63d183
jonesguidini <jonesguidini@gmail.com>
2020-11-06 12:01:29 -0300

    'NSE.Identitdade.API'

---
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Erros = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                { "Mensagens", Erros.ToArray() }
+            }));
+        }
+
+        /// <summary>
+        /// Validação de erros da ViewModel do Entity
+        /// </summary>
+        /// <param name="modelState"></param>
+        /// <returns></returns>
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var erro in erros)
+            {
+                AdicionarErroProcessamento(erro.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !Erros.Any();
+        }
+
+        protected void AdicionarErroProcessamento(string erro)
+        {
+            Erros.Add(erro);
+        }
+
+        protected void LimparErrosProcessamento()
+        {
+            Erros.Clear();
+        }
+    }
+}
