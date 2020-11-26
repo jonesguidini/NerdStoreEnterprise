3633097cab0411145d5b0c06122b080998969483
jonesguidini <jonesguidini@gmail.com>
2020-11-26 16:27:12 -0300

    Implementacoes api clientes

---
+++ src/services/NSE.Cliente.API/Data/ClientesContext.cs
@@ -0,0 +1,45 @@
+﻿using NSE.Core.Data;
+using NSE.Clientes.API.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Clientes.API.Data
+{
+    public sealed class ClientesContext : DbContext, IUnitOfWork
+    {
+        public ClientesContext(DbContextOptions<ClientesContext> options) : base(options)
+        {
+            // melhorar a performance
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Ignore<ValidationResult>();
+            //modelBuilder.Ignore<Event>();
+
+            // esse recurso mapeia as strings caso não mapeado
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            // configura / desabilita o delete em cascata
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            var sucesso = await base.SaveChangesAsync() > 0;
+            //if (sucesso) await _mediatorHandler.PublicarEventos(this);
+
+            return sucesso;
+        }
+    }
+}
